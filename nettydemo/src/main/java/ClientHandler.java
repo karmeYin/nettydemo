58b47cf47722c1d02cfe5216764b7cb167ec30cf
Mark <893888505@qq.com>
2018-04-09 21:50:08 +0800

    一个简单的demo

---
+++ nettydemo/src/main/java/ClientHandler.java
@@ -0,0 +1,28 @@
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.ByteBufUtil;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandler;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.util.CharsetUtil;
+import io.netty.util.concurrent.EventExecutorGroup;
+
+import java.nio.ByteBuffer;
+
+public class ClientHandler extends SimpleChannelInboundHandler<ByteBuf> {
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) {
+        ctx.writeAndFlush(Unpooled.copiedBuffer("Netty rocks!",CharsetUtil.UTF_8));
+    }
+    @Override
+    public void channelRead0(ChannelHandlerContext ctx, ByteBuf in) throws  Exception{
+        System.out.println("Client received: " + ByteBufUtil.hexDump(in.readBytes(in.readableBytes())));
+    }
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx,
+                                Throwable cause) {
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
+
